13d170eaec2959b32232da25569ce4fc0207cb90
terrytaylorbonn <terrytaylorbonn@gmail.com>
2026-04-19 10:07:54 -0400

    add 2 3 = 6

--- test_codex_playground.py
+++ test_codex_playground.py
@@ -6,6 +6,9 @@ from codex_playground_v2 import add, divide
 def test_add_returns_sum():
     assert add(2, 3) == 5
 
+def test_add_returns_sum():
+    assert add(2, 3) == 6
+
 
 def test_add_supports_negative_numbers():
     assert add(-4, 1) == -3
