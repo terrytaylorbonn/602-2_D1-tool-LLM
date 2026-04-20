66b88dafa272784bd8e90bde43f6946e8486c224
terrytaylorbonn <terrytaylorbonn@gmail.com>
2026-04-20 13:51:15 -0400

    webhoook

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"ok": True, "message": "hello from Render!"}
+
+from fastapi import Request
+
+@app.post("/github-webhook")
+async def github_webhook(request: Request):
+    data = await request.json()
+    print("EVENT:", request.headers.get("X-GitHub-Event"))
+    print("PAYLOAD_KEYS:", list(data.keys())[:10])
+    return {"ok": True}
